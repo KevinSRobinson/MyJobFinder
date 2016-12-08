031d80943e47c5543cbce9ac0d19870584dc0228
seamusjunior44 <kevin.robinson@outlook.com>
2016-12-08 18:00:41 +0000

    root

---
+++ public/app/app.js
@@ -0,0 +1,14 @@
+angular.module('app', []);
+
+angular.module('app').controller('testCtrl', function($scope) {
+
+    $scope.jobs = [{
+            title: 'Sales Person',
+            description: 'Desctiotuinsd  sdfsdf  sdfsfsf sdfsfd f'
+        }, {
+            title: 'Accounts',
+            description: ' dd dddctiotuinsd  sdfsdf  sdfsfsf sdfsfd f'
+        },
+
+    ]
+});
