9b1c94fc126c1f2b1a8d502ba601e4955336a953
seamusjunior44 <kevin.robinson@outlook.com>
2016-12-10 12:57:08 +0000

    added jade module

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var errorHandler = require('./errorHandler')();
 var app = express();
-var jobModel = require('./models/Job');
+var jobModel = require('./models/jJob');
 
 
 app.set('view engine', 'jade');
